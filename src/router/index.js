0a1e9a4d1043940100dcd2b41fb88226670b82aa
hwi0523 <101933886+hwi0523@users.noreply.github.com>
2022-07-19 18:05:21 +0900

    0719 8교시

--- src/router/index.js
+++ src/router/index.js
@@ -1,48 +1,61 @@
 import { createRouter, createWebHistory } from 'vue-router';
+
 import ProductList from '../views/ProductList';
 import ProductDetail from '../views/ProductDetail';
 import ProductCreate from '../views/ProductCreate';
 import ProductUpdate from '../views/ProductUpdate';
 import SalesList from '../views/SalesList';
 import ImageInsert from '../views/ImageInsert';
+import store from '@/store';
+import swal from 'sweetalert2';
 
-const routes = [
+//네비게이션 가드
+const requireAuth = () => (to, from, next) => {
+    if(store.state.user.iuser === undefined) {    
+      swal.fire('로그인을 하세요.', '', 'warning');
+      return;  
+    }
+    next();
+}
+
+const routes = [  
   {
-    path: '/list',
-    name: 'ProductList',
+    path: '/',
+    name: 'Home',    
     component: ProductList
   },
   {
     path: '/detail',
-    name: 'ProductDetail',
+    name: 'ProductDetail',    
     component: ProductDetail
   },
   {
     path: '/create',
     name: 'ProductCreate',
-    component: ProductCreate
+    component: ProductCreate,
+    beforeEnter: requireAuth()
   },
   {
     path: '/update',
-    name: 'ProductUpdate',
+    name: 'ProductUpdate',    
     component: ProductUpdate
   },
   {
     path: '/sales',
-    name: 'SalesList',
+    name: 'SalesList',    
     component: SalesList
   },
   {
     path: '/image_insert',
-    name: 'ImageInsert',
-    component: ImageInsert
-  },
-  
+    name: 'ImageInsert',    
+    component: ImageInsert,
+    beforeEnter: requireAuth()
+  }
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
-})
+});
 
-export default router
+export default router;
